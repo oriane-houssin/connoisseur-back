06e18d7a91bba2221b1c582666b847345510574c
oriane-houssin <117643200+oriane-houssin@users.noreply.github.com>
2025-05-16 15:39:06 -0400

    Connexion à la BDD mysql

---
+++ db.js
@@ -0,0 +1,12 @@
+const mysql = require('mysql2/promise');
+// promise permet d'utiliser async/await au lieu de callbacks
+require('dotenv').config();
+
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+});
+
+module.exports = connection;
