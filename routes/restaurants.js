eafeebfc318861c2605e0b571e3be1f6edf880d2
oriane-houssin <117643200+oriane-houssin@users.noreply.github.com>
2025-05-26 09:58:01 -0400

    MAJ data import lors d'appel API restaurants

--- routes/restaurants.js
+++ routes/restaurants.js
@@ -17,25 +17,57 @@ router.get('/latest', async (req, res) => {
             id: r.recordid,
             name: r.fields.name,
             cuisine: r.fields.cuisine || null,
-            vegetarian: r.fields.vegetarian || null,
-            vegan: r.fields.vegan || null,
-            opening_hours: r.fields.opening_hours || null,
-            wheelchair: r.fields.wheelchair || null,
-            delivery: r.fields.delivery || null,
-            takeaway: r.fields.takeaway || null,
             michelin: r.fields.stars || null,
-            phone: r.fields.phone || null,
-            website: r.fields.website || null,
             city: r.fields.meta_name_com || null,
             brand: r.fields.brand || null,
-            date: r.record_timestamp,
             rating: Math.floor(Math.random() * 5) + 1
         }));
         res.json(restaurants);
     } catch (error) {
-        console.error('Erreur API ODS:', error.message);
-        res.status(500).json({error: 'Erreur lors de la récupération des données'});
+        res.status(500).json({error: 'Erreur serveur'});
     }
 });
 
 module.exports = router;
+
+// Récupère un restaurant depuis l'id des params
+router.get('/:id', async (req, res) => {
+    const id = req.params.id;
+    try {
+        const response = await axios.get('https://public.opendatasoft.com/api/records/1.0/search/', {
+            params: {
+                dataset: 'osm-france-food-service',
+                q: id,
+                rows: 1
+            }
+        });
+        if (response.data.records.length === 0) {
+            return res.status(404).json({error: 'Restaurant not found'})
+        }
+        const r = response.data.records[0];
+        res.json({
+            id: r.recordid,
+            name: r.fields.name,
+            cuisine: r.fields.cuisine || 'Non spécifiée',
+            vegetarian: r.fields.vegetarian || null,
+            vegan: r.fields.vegan || null,
+            opening_hours: r.fields.opening_hours || "Non spécifiés",
+            wheelchair: r.fields.wheelchair || null,
+            delivery: r.fields.delivery || "Non spécifié",
+            takeaway: r.fields.takeaway || "Non spécifié",
+            michelin: r.fields.stars || null,
+            phone: r.fields.phone || "Non spécifié",
+            website: r.fields.website || null,
+            city: r.fields.meta_name_com || null,
+            department: r.fields.meta_name_dep || null,
+            departmentCode: r.fields.meta_code_dep || null,
+            street: r.fields['addr:street'] || null,
+            housenumber: r.fields['addr:housenumber'] || null,
+            brand: r.fields.brand || "Indépendant",
+            date: r.record_timestamp,
+            rating: Math.floor(Math.random() * 5) + 1
+        });
+    } catch (error) {
+        res.status(500).json({error: 'Erreur serveur'});
+    }
+})
